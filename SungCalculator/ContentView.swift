f4621e4a713f8cadb8f7b592162e21e17d62fb04
TSanni <tomas.sanni@gmail.com>
2022-12-26 01:45:45 -0600

    Initial Commit

---
+++ SungCalculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SungCalculator
+//
+//  Created by Tomas Sanni on 12/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SungCalculator/SungCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SungCalculatorApp.swift
+//  SungCalculator
+//
+//  Created by Tomas Sanni on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SungCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

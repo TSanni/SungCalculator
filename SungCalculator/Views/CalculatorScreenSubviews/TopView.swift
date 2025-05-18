07230c30d3bfd9659ff8e8fdae5252fd0a880d02
Tomas Sanni <tomas.sanni7@gmail.com>
2025-05-18 12:21:58 -0500

    replacement started

---
+++ SungCalculator/Views/CalculatorScreenSubviews/TopView.swift
@@ -0,0 +1,18 @@
+//
+//  TopView.swift
+//  SungCalculator
+//
+//  Created by Tomas Sanni on 7/6/24.
+//
+
+import SwiftUI
+
+struct TopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopView()
+}
---
+++ SungCalculator/Views/Screens/MainScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreen.swift
+//  SungCalculator
+//
+//  Created by Tomas Sanni on 5/17/25.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainScreen()
+}
